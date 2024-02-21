034ccc3688bf4ddc40d7e11d00c6fe9598661608
obula sai mochi <110908237+obula-sai@users.noreply.github.com>
2024-02-21 20:05:38 +0530

    Delete backend directory

--- backend/products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from .views import *
-
-urlpatterns = [
-    path('' , homeview , name = "home"),
-    path('products' , ProductList.as_view()),
-    path('product/<int:pk>' , ProductDetailView.as_view() , name = "product-detail"),
-]
